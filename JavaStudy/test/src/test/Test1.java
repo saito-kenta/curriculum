73319d85b9042071af4199ccfceded510dc26e87
saito-kenta <99403695+saito-kenta@users.noreply.github.com>
2022-02-18 13:31:45 +0900

    TEST
    TEST

---
+++ JavaStudy/test/src/test/Test1.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		sayHelllo("太郎", 30);
+	}
+
+	// sayHelloメソッド
+	public static void sayHelllo(String name, int age) {
+		System.out.println("Hello" + name + "さん。年齢は" + age + "歳です。" );
+	}
+
+}
---
+++ JavaStudy/test/src/test/test2.java
@@ -0,0 +1,16 @@
+package test;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		getName("齋藤", "健太");
+	}
+
+	// getNameメソッド
+	public static void getName(String firstName, String lastName) {
+		String fullName = firstName + lastName;
+		System.out.println("私の名前は" + fullName + "です。");
+	}
+
+}
