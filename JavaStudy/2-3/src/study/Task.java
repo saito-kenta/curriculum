bbc7bc549adcf836f7893e0f3aa5dd94a3fd42b5
saito-kenta <99403695+saito-kenta@users.noreply.github.com>
2022-03-08 19:57:40 +0900

    課題提出テスト
    Main.java、Task.java、Calculator.java の作成

---
+++ JavaStudy/2-3/src/study/Task.java
@@ -0,0 +1,20 @@
+package study;
+
+// ① TaskクラスにCalculatorクラスを継承させなさい。
+
+/**
+ * タスクの実行
+ */
+public class Task extends Calculator {
+	public void doTask() {
+
+		// ② Calculator.javaのすべてのオーバーロードメソッド「plus」に適当な引数を与え、下記画像のよう出力されるようコーディングしなさい。
+		// 尚、「どのクラスから呼び出しているか」を明確にするため、plus()には呼び出し元のキーワードを付与すること。
+		int sumValue = plus(10);
+		int sumValue2 = plus(10, 20);
+		int sumValue3 = plus(10, 20, 30);
+		System.out.println("plusメソッドの引数が一つの場合：" + sumValue);
+		System.out.println("plusメソッドの引数が二つの場合：" + sumValue2);
+		System.out.println("plusメソッドの引数が三つの場合：" + sumValue3);
+	}
+}
