80126e2450a5c14928980319fff4adae39e66cb3
saito-kenta <99403695+saito-kenta@users.noreply.github.com>
2022-03-16 14:38:22 +0900

    課題提出テスト
    Task.javaの修正

--- JavaStudy/2-3/src/study/Task.java
+++ JavaStudy/2-3/src/study/Task.java
@@ -10,9 +10,9 @@ public class Task extends Calculator {
 
 		// ② Calculator.javaのすべてのオーバーロードメソッド「plus」に適当な引数を与え、下記画像のよう出力されるようコーディングしなさい。
 		// 尚、「どのクラスから呼び出しているか」を明確にするため、plus()には呼び出し元のキーワードを付与すること。
-		int sumValue = plus(10);
-		int sumValue2 = plus(10, 20);
-		int sumValue3 = plus(10, 20, 30);
+		int sumValue = super.plus(10);
+		int sumValue2 = super.plus(10, 20);
+		int sumValue3 = super.plus(10, 20, 30);
 		System.out.println("plusメソッドの引数が一つの場合：" + sumValue);
 		System.out.println("plusメソッドの引数が二つの場合：" + sumValue2);
 		System.out.println("plusメソッドの引数が三つの場合：" + sumValue3);
